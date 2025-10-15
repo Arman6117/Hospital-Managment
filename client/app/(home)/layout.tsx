d3441ec1bde6c83f6b6d0d1976ca178cba1ce3b2
Arman Patel <vanixgaming634@gmail.com>
2025-10-15 20:19:41 +0530

    feat: Add dashboard page and layout components with new UI elements

---
+++ client/app/(home)/layout.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/navbar";
+import React from "react";
+
+const HomeLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="h-screen w-screen bg-gradient-to-br from-slate-50 via-gray-50 to-blue-50/30">
+      <Navbar />
+      {children}
+    </main>
+  );
+};
+
+export default HomeLayout;
